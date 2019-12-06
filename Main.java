ae0d775a62f56f64185ec5eda112aa50e2e1b862
raenalyn <58575755+raenalyn@users.noreply.github.com>
2019-12-06 08:34:19 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
+   
   public static void main(String[] args) {
-    System.out.println("Hello World");
+      String name = "Rae"; 
+    System.out.println("Hello World" + name);
   }
 
 }
